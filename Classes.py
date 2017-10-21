aed9f34752f57e95bab5fa8aa67613042bbfee5f
Unknown <andert.noah@gmx.at>
2017-10-22 01:53:45 +0200

    Classes und Loops Dateien wurden geaddet

---
+++ Classes.py
@@ -0,0 +1,27 @@
+
+class myClass():
+
+    def method1(self): #Self-Argument weist auf das Object an sich hin.
+        print("myClass Method1")
+
+    def method2(self, someString):
+        print("myClass Method2 " + someString)
+
+class anotherClass(myClass): #anotherClass wird inheritiert von myClass
+    def method2(self): #Methoden werden Overrided
+        print("anotherClass Method2")
+
+    def method1(self):
+        myClass.method1(self);
+        print("anotherClass method1")
+
+def main():
+
+    c = myClass() #Klasse wird als Object instanziert
+    c.method1() #Methode1 wird gecalled
+    c.method2("This is a String") #Man muss kein Parameter für self übergeben. Das macht Python schon selber
+    c2 = anotherClass()
+    c2.method1()
+
+if __name__ == "__main__":
+    main()
--- loops.py
+++ loops.py
@@ -7,7 +7,20 @@ def main():
 		print (x)
 		x = x + 1 
 
+	for x in range(5,10):
+		print(x)
 
+	days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+	for d in days: #For Loop wird über jeden Index iterieren
+		print (d)
+
+	for x in range(5,10):
+		#if (x==7): break
+		if (x % 2 == 0 ): continue #Continue skippt den Rest der Schleife und springt direkt an den Anfang zurück
+		print(x)
+
+	for i, d in enumerate(days): #i Wäre in diesem Fall der Index des Items.
+		print(i, d)
 
 if __name__ == "__main__":
 	main()
